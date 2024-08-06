d04ca1796d5818b18113fd5b2f26015b12c6d5a5
Shailesh <shaileshwaran714@gmail.com>
2024-08-06 15:12:45 +0530

    revert changes

--- Backend/setup.sh
+++ Backend/setup.sh
@@ -1,43 +1,12 @@
 #!/bin/bash
 
 # Install Ollama
-wget -qO- https://ollama.com/install.sh | sed 's#https://ollama.com/download#https://github.com/jmorganca/ollama/releases/download/v0.1.27#' | sh
+curl -fsSL https://ollama.com/install.sh | sed 's#https://ollama.com/download#https://github.com/jmorganca/ollama/releases/download/v0.1.27#' | sh
 
 # Install Python packages
-pip install requests accelerate
+pip install ngrok pyngrok requests accelerate flask
 
-# Function to start a process
-start_process() {
-  command=$1
-  $command > >(tee /dev/stderr) 2>&1 &
-  echo $!  # Return the PID of the process
-}
+ngrok config add-authtoken 2fb0g0mRnwBSSNVgVy8lBrhJF8M_2R1xDsvtZjMpHGFe7DvkZ
+# Start ngrok service
+python Startup.py 
 
-# Function to stop a process
-stop_process() {
-  pid=$1
-  kill $pid
-  wait $pid 2>/dev/null
-}
-
-# Start Ollama service
-ollama_command="ollama serve"
-ollama_pid=$(start_process "$ollama_command")
-
-# Wait a bit to ensure the Ollama service starts
-sleep 5
-
-# Run the ollama pull command
-ollama pull llama3.1
-
-# Doctors list
-doctors="psychatrist gynaecologist pediatrician dermatologist"
-
-# Loop through each item and create the configurations
-for doc in $doctors
-do
-  ollama create $doc -f ./configFiles/$doc.txt
-done
-
-# Stop the Ollama service
-stop_process $ollama_pid
