661302f7c9bcd7971493e1646e0d11fae9021bec
Shailesh <shaileshwaran714@gmail.com>
2024-08-06 14:27:02 +0530

    ngrok change

--- Backend/setup.sh
+++ Backend/setup.sh
@@ -3,7 +3,7 @@
 # Install Ollama
 curl -fsSL https://ollama.com/install.sh | sed 's#https://ollama.com/download#https://github.com/jmorganca/ollama/releases/download/v0.1.27#' | sh
 
-ngrok config add-authtoken 2fb0g0mRnwBSSNVgVy8lBrhJF8M_2R1xDsvtZjMpHGFe7DvkZ
 # Install Python packages
 pip install ngrok pyngrok requests accelerate flask
 
+ngrok config add-authtoken 2fb0g0mRnwBSSNVgVy8lBrhJF8M_2R1xDsvtZjMpHGFe7DvkZ
