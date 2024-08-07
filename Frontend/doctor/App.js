f3d3cafad51c356bf9e135220b0b879e4b5f52d9
Shailesh <shaileshwaran714@gmail.com>
2024-08-07 14:03:24 +0530

    shailesh

--- Frontend/doctor/App.js
+++ Frontend/doctor/App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import ChatScreen from "./app/screens/ChatScreen";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <>
+    <SafeAreaView>
       <ChatScreen />
-    </>
+    </SafeAreaView>
   );
 }
 
