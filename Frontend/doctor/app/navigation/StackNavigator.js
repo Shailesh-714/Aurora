77ea6055291a16e1eeb7da6c0b10af6b8413bc1d
Shailesh <shaileshwaran714@gmail.com>
2024-08-12 19:37:18 +0530

    stack navigator paddingtop added

--- Frontend/doctor/app/navigation/StackNavigator.js
+++ Frontend/doctor/app/navigation/StackNavigator.js
@@ -1,13 +1,14 @@
-import { StyleSheet } from "react-native";
+import { Dimensions, Platform, StyleSheet, View } from "react-native";
 import React from "react";
 import AnimTab1 from "./BottomTabs"; // Adjust the path as necessary
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
+const {width} = Dimensions.get("window");
 
 const Stack = createNativeStackNavigator();
 
 const StackNavigator = () => {
-  return (
+  return (<View style={{...Platform.select({android:{paddingTop:width*0.025}})}}>
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen
@@ -16,7 +17,7 @@ const StackNavigator = () => {
           options={{ headerShown: false }}
         />
       </Stack.Navigator>
-    </NavigationContainer>
+    </NavigationContainer></View>
   );
 };
 
