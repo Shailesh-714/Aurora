8fd440acbf8913a2ab7a98d0d4810748b2084d81
shamyugtha-kr <138640352+shamyugtha-kr@users.noreply.github.com>
2024-10-02 21:06:10 +0530

    Logs

--- Frontend/doctor/app/screens/Dashboard.js
+++ Frontend/doctor/app/screens/Dashboard.js
@@ -18,6 +18,7 @@ import {
   MaterialIcons,
   FontAwesome5,
   Ionicons,
+  FontAwesome6,
 } from "@expo/vector-icons";
 
 const chatList = [
@@ -432,7 +433,7 @@ const Dashboard = () => {
                       progress={0.4}
                       borderWidth={0}
                       thickness={2}
-                      color="#3dc02d"
+                      color="#18a561"
                       strokeCap="round"
                       showsText={false}
                     />
@@ -466,7 +467,7 @@ const Dashboard = () => {
                       progress={0.7}
                       borderWidth={0}
                       thickness={2}
-                      color="#3dc02d"
+                      color="#18a561"
                       strokeCap="round"
                       showsText={false}
                     />
@@ -522,6 +523,99 @@ const Dashboard = () => {
                 <View style={{ alignItems: "center", margin: 5 }}>
                   <Text style={{ color: "grey", fontSize: 12 }}>Fibre</Text>
                 </View>
+              </View>
+            </View>
+          </View>
+          <View
+            style={{
+              backgroundColor: "white",
+              borderRadius: 20,
+              padding: 30,
+              marginVertical: 20,
+              rowGap: 25,
+            }}
+          >
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <View style={{ flexDirection: "row" }}>
+                <View style={{ alignSelf: "center" }}>
+                  <FontAwesome6 name="glass-water" size={24} color="#228bc7" />
+                </View>
+                <View
+                  style={{ justifyContent: "space-evenly", marginLeft: "15%" }}
+                >
+                  <Text style={{ fontSize: 16, fontWeight: "bold" }}>
+                    Water
+                  </Text>
+                  <Text style={{ fontSize: 12, color: "grey" }}>
+                    3 of 6 Glasses
+                  </Text>
+                </View>
+              </View>
+
+              <View>
+                <AntDesign name="pluscircleo" size={18} color="black" />
+              </View>
+            </View>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <View style={{ flexDirection: "row" }}>
+                <View style={{ alignSelf: "center" }}>
+                  <Ionicons
+                    name="cloudy-night-sharp"
+                    size={24}
+                    color="#8856b5"
+                  />
+                </View>
+                <View
+                  style={{ justifyContent: "space-evenly", marginLeft: "10%" }}
+                >
+                  <Text style={{ fontSize: 16, fontWeight: "bold" }}>
+                    Sleep
+                  </Text>
+                  <Text style={{ fontSize: 12, color: "grey" }}>
+                    7 hr 30 min of 8 hr
+                  </Text>
+                </View>
+              </View>
+
+              <View>
+                <AntDesign name="pluscircleo" size={18} color="black" />
+              </View>
+            </View>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <View style={{ flexDirection: "row" }}>
+                <View style={{ alignSelf: "center" }}>
+                  <FontAwesome6 name="weight-scale" size={24} color="#ae3232" />
+                </View>
+                <View
+                  style={{ justifyContent: "space-evenly", marginLeft: "15%" }}
+                >
+                  <Text style={{ fontSize: 16, fontWeight: "bold" }}>
+                    Weight
+                  </Text>
+                  <Text style={{ fontSize: 12, color: "grey" }}>49 kgs</Text>
+                </View>
+              </View>
+
+              <View>
+                <AntDesign name="pluscircleo" size={18} color="black" />
               </View>
             </View>
           </View>
